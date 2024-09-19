e0e4c718fbea4b33fa40f93312e76bb6f19b24ca
t-motegi <motegi.web@gmail.com>
2024-09-19 12:19:23 +0900

    🎨 update：アプリケーション層のエラーハンドリングの実装

--- src/application/service/turnService.ts
+++ src/application/service/turnService.ts
@@ -1,9 +1,9 @@
-import { GameGateway } from "../infrastructure/gameGateway";
-import { connectMySQL } from "../infrastructure/connection";
-import { toDisc } from "../domain/model/turn/disc";
-import { Point } from "../domain/model/turn/point";
-import { TurnRepository } from "../domain/model/turn/turnRepository";
-import { GameRepository } from "../domain/model/game/gameRepository";
+import { connectMySQL } from "../../infrastructure/connection";
+import { toDisc } from "../../domain/model/turn/disc";
+import { Point } from "../../domain/model/turn/point";
+import { TurnRepository } from "../../domain/model/turn/turnRepository";
+import { GameRepository } from "../../domain/model/game/gameRepository";
+import { ApplicationError } from "../error/applicationError";
 
 const turnRepository = new TurnRepository();
 const gameRepository = new GameRepository();
@@ -41,7 +41,10 @@ export class TurnService {
     try {
       const game = await gameRepository.findLatest(conn);
       if (!game) {
-        throw new Error("Latest game not found");
+        throw new ApplicationError(
+          "LatestGameNotFound",
+          "Latest game not found"
+        );
       }
       if (!game.id) {
         throw new Error("Game ID not exist");
@@ -70,7 +73,10 @@ export class TurnService {
     try {
       const game = await gameRepository.findLatest(conn);
       if (!game) {
-        throw new Error("Latest game not found");
+        throw new ApplicationError(
+          "LatestGameNotFound",
+          "Latest game not found"
+        );
       }
       if (!game.id) {
         throw new Error("Game ID not exist");
